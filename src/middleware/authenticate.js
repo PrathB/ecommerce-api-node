3fa3db7fc21a10188724537051769d721821fa46
Pratham Batra <pratham11.batra@gmail.com>
2024-10-19 22:26:07 +0530

    Created authenticate.js (middleware) and adminController route

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,18 @@
+const JwtProvider = require("../config/jwtProvider");
+const UserService = require("../services/user.service");
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(404).send({ error: "JWT token not found" });
+    }
+    const user = await UserService.getUserProfileByToken(token);
+    req.user = user;
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+  next();
+};
+
+module.exports = { authenticate };
