883d8f43d26e84b2da7a9d8baea51d3663444994
Pratham Batra <pratham11.batra@gmail.com>
2024-10-19 13:52:20 +0530

    Created order controller for user

---
+++ src/controller/order.controller.js
@@ -0,0 +1,32 @@
+const OrderService = require("../services/order.service");
+
+const createOrder = async (req, res) => {
+  const user = req.user;
+  try {
+    const createdOrder = await OrderService.createOrder(user._id, req.body);
+    return res.status(201).send(createdOrder);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const findOrderById = async (req, res) => {
+  try {
+    const order = await OrderService.findOrderById(req.params.orderId);
+    return res.status(200).send(order);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const getUserOrderHistory = async (req, res) => {
+  const user = req.user;
+  try {
+    const orders = await OrderService.getUserOrderHistory(user._id);
+    return res.status(200).send(orders);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = { createOrder, findOrderById, getUserOrderHistory };
