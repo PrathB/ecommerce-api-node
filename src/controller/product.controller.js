ff5f0a2c62eca773a70b735630e04299f259c98e
Pratham Batra <pratham11.batra@gmail.com>
2024-10-19 14:11:19 +0530

    Created product controller

---
+++ src/controller/product.controller.js
@@ -0,0 +1,70 @@
+const ProductService = require("../services/product.service");
+
+const createProduct = async (req, res) => {
+  try {
+    const product = await ProductService.createProduct(req.body);
+    return res.status(201).send(product);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const productId = req.params.productId;
+  try {
+    const message = await ProductService.deleteProduct(productId);
+    return res.status(200).send({ message: message });
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const productId = req.params.productId;
+  try {
+    const updatedProduct = await ProductService.updateProduct(
+      productId,
+      req.body
+    );
+    return res.status(200).send(updatedProduct);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const findProductById = async (req, res) => {
+  const productId = req.params.productId;
+  try {
+    const product = await ProductService.findProductById(productId);
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await ProductService.getAllProducts(req.query);
+    return res.status(200).send(products);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const createMultipleProducts = async (req, res) => {
+  try {
+    await ProductService.createMultipleProducts(req.body);
+    return res.status(201).send({message:"Products created successfully"});
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  findProductById,
+  getAllProducts,
+  createMultipleProducts,
+};
